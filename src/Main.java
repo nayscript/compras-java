1cd1ac75ac32dfcec4a9efcca6212ddfb85a620f
Nayrlen Alves <161717698+nayscript@users.noreply.github.com>
2024-05-22 09:34:00 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,48 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int limite;
-        String descricao;
-        int valorDaCompra;
 
-        for (opcao =! 2){
-            Scanner leitura = new Scanner(System.in);
-            System.out.println("Digite o limite do cartão:");
-            limite = leitura.nextInt();
+        Scanner leitura = new Scanner(System.in);
 
+        System.out.println("Digite o limite do cartão:");
+        double limite = leitura.nextInt();
+
+        CartaoDeCredito cartao = new CartaoDeCredito(limite);
+
+        int sair = 1;
+
+        while (sair != 0) {
             System.out.println("Digite a descrição da compra:");
-            descricao = leitura.nextLine();
+            String descricao = leitura.nextLine();
 
             System.out.println("Digite o valor da compra:");
-            valorDaCompra = leitura.nextInt();
+            double valorDaCompra = leitura.nextDouble();
 
             System.out.println("Compra realizada!");
             System.out.println("Didite 1 para sair ou 0 para continuar.");
-            opcao = leitura.nextInt();
+
+            Compra compra = new Compra(descricao, valorDaCompra);
+            boolean compraRealizada = cartao.lancaCompra(compra);
+
+            if (compraRealizada) {
+                System.out.println("Compra realizada!");
+                System.out.println("Digite 0 para sair ou 1 para continuar");
+                sair = leitura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                sair = 0;
+            }
         }
+
+        System.out.println("*********************************");
+        System.out.println("COMPRAS REALIZADAS:\n");
+
+        for (Compra c : cartao.getCompras()) {
+            System.out.println(c.getDescricao() + " - " +c.getValor());
+        }
+
+        System.out.println("\n*********************************");
+
+        System.out.println("\nSaldo do cartão: " +cartao.getSaldo());
     }
 }
